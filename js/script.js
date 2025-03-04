9c0c2b997408f9e6f54ac72b3d051558854169dd
Akas_Datta <012akasdatta@gmail.com>
2025-03-05 03:31:07 +0600

    added navbar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.getElementById('toggleMenu');
     const menu = document.getElementById('navbar-sticky');
-    const plusIcon = toggleButton.querySelector('.fa-plus');
+    const barsIcon = toggleButton.querySelector('.fa-bars');
     const xmarkIcon = toggleButton.querySelector('.fa-xmark');
 
     toggleButton.addEventListener('click', function () {
         menu.classList.toggle('hidden'); // Show/hide the menu
-        plusIcon.classList.toggle('hidden'); // Toggle plus icon
+        barsIcon.classList.toggle('hidden'); // Toggle bars icon
         xmarkIcon.classList.toggle('hidden'); // Toggle x-mark icon
     });
 
